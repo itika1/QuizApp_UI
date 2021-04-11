69c8a51484bedb5101e807f81bf9aa3b80c48052
Itika Sarkar <itikaitzme@gmail.com>
2021-04-11 15:52:29 +0530

    deploy my react UI app to github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../node_modules/@mdi/font/css/materialdesignicons.min.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
+import './styles/styles.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
